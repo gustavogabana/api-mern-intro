0a11a34f448b919bc4ec2726d0d877fab4b59a53
gustavogabana <gustavo_97@protonmail.com>
2024-01-23 19:47:19 -0300

    refactor: api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,18 @@
 const create = (req, res) => {
-    const user = req.body
-    res.json(user)
+    const {name, username, email, password, avatar, background} = req.body
+    if (!name || !username || !email || !password || !avatar || !background) {
+        return res.status(400).send({message: 'Submit all fields for registration'})
+    }
+    return res.status(201).send({
+        message: 'Created',
+        user: {
+            name,
+            username,
+            email,
+            avatar,
+            background
+        }
+    })
 }
 
 module.exports = { create }

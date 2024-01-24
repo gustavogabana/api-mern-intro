1ee132e00f77889c63e347152691c1f0075bd4f0
gustavogabana <gustavo_97@protonmail.com>
2024-01-23 21:01:52 -0300

    Connection successfully

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,22 @@
-const create = (req, res) => {
+const userService = require('../services/user.service')
+
+const create = async (req, res) => {
     const {name, username, email, password, avatar, background} = req.body
+
     if (!name || !username || !email || !password || !avatar || !background) {
         return res.status(400).send({message: 'Submit all fields for registration'})
     }
+
+    const user = await userService.create(req.body)
+
+    if (!user) {
+        return res.status(400).send({message: 'Error creating User'})
+    }
+
     return res.status(201).send({
         message: 'Created',
         user: {
+            id: user._id,
             name,
             username,
             email,
@@ -13,6 +24,7 @@ const create = (req, res) => {
             background
         }
     })
+
 }
 
 module.exports = { create }

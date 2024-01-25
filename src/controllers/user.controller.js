d0442f50fffd97efa7351253d8acf406ae5eb612
gustavogabana <gustavo_97@protonmail.com>
2024-01-25 20:14:40 -0300

    Patch method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -51,4 +51,29 @@ const findById = async (req, res) => {
     res.send(user)
 }
 
-module.exports = { create, findAll, findById }
+const update = async (req, res) => {
+    const {name, username, email, password, avatar, background} = req.body
+
+    if (!name && !username && !email && !password && !avatar && !background) {
+        return res.status(400).send({message: 'Submit at least one field for update'})
+    }
+
+    const id = req.params.id
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({message: 'Invalid id'})
+    }
+
+    const user = await userService.findByIdService(id)
+
+    if (!user) {
+        return res.status(400).send({message: 'User not found by this id'})
+    }
+
+    await userService.updateService(id, name, username, email, password, avatar, background)
+
+    return res.status(200).send({message: 'User successfully updated'})
+
+}
+
+module.exports = { create, findAll, findById, update }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/user.controller')
 route.post('/user', userController.create)
 route.get('/user', userController.findAll)
 route.get('/user/:id', userController.findById)
+route.patch('/user/:id', userController.update)
 
 module.exports = route

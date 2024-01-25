1d8927c404b16e37ac58519bc79e03e5429c7862
gustavogabana <gustavo_97@protonmail.com>
2024-01-24 21:58:09 -0300

    Get all and get by id

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userService = require('../services/user.service')
+const mongoose = require('mongoose')
 
 const create = async (req, res) => {
     const {name, username, email, password, avatar, background} = req.body
@@ -7,7 +8,7 @@ const create = async (req, res) => {
         return res.status(400).send({message: 'Submit all fields for registration'})
     }
 
-    const user = await userService.create(req.body)
+    const user = await userService.createService(req.body)
 
     if (!user) {
         return res.status(400).send({message: 'Error creating User'})
@@ -27,4 +28,27 @@ const create = async (req, res) => {
 
 }
 
-module.exports = { create }
+const findAll = async (req, res) => {
+    const users = await userService.findAllService()
+
+    if (users.length === 0) {
+        return res.status(400).send({message: 'There are no registered users'})
+    }
+    res.send(users)
+}
+
+const findById = async (req, res) => {
+
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).send({message: 'Invalid id'})
+    }
+
+    const user = await userService.findByIdService(req.params.id)
+
+    if (!user) {
+        return res.status(400).send({message: 'User not found by this id'})
+    }
+    res.send(user)
+}
+
+module.exports = { create, findAll, findById }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,5 +2,7 @@ const route = require('express').Router()
 const userController = require('../controllers/user.controller')
 
 route.post('/user', userController.create)
+route.get('/user', userController.findAll)
+route.get('/user/:id', userController.findById)
 
 module.exports = route

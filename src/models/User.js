f968616fa0d5a533e962ea7b919a7a64a10b8826
gustavogabana <gustavo_97@protonmail.com>
2024-01-26 20:06:42 -0300

    Bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -12,11 +13,13 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        select: false
     },
     avatar: {
         type: String,
@@ -26,6 +29,11 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+})
+
+UserSchema.pre('save', async function(next) {
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
 })
 
 const User = mongoose.model('User', UserSchema)

7f53d57b8313c145a3f94f08ea89bcc94501f942
gustavogabana <gustavo_97@protonmail.com>
2024-01-25 21:05:44 -0300

    Try catch middleware

--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -2,22 +2,30 @@ const mongoose = require('mongoose')
 const userService = require('../services/user.service')
 
 const validId = (req, res, next) => {
-    const id = req.params.id
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).send({message: 'Invalid id'})
+    try {
+        const id = req.params.id
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({message: 'Invalid id'})
+        }
+        next()
+    } catch (err) {
+        return res.status(500).send({message: err.message})
     }
-    next()
 }
 
 const validUser = async (req, res, next) => {
-    const id = req.params.id
-    const user = await userService.findByIdService(id)
-    if (!user) {
-        return res.status(400).send({message: 'User not found by this id'})
+    try {
+        const id = req.params.id
+        const user = await userService.findByIdService(id)
+        if (!user) {
+            return res.status(400).send({message: 'User not found by this id'})
+        }
+        req.id = id
+        req.user = user
+        next()
+    } catch (err) {
+        res.status(500).send({message: err.message})
     }
-    req.id = id
-    req.user = user
-    next()
 }
 
 module.exports = {validId, validUser}

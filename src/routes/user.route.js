90934f9dfa579e13796a2d3c9425b340c9e568f5
gustavogabana <gustavo_97@protonmail.com>
2024-01-29 20:35:43 -0300

    User.route refactor

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,9 +3,9 @@ import userController from '../controllers/user.controller.js'
 import middleware from '../middlewares/global.middleware.js'
 
 const router = Router()
-router.post('/user', userController.create)
-router.get('/user', userController.findAll)
-router.get('/user/:id', middleware.validId, middleware.validUser, userController.findById)
-router.patch('/user/:id', middleware.validId, middleware.validUser, userController.update)
+router.post('/', userController.create)
+router.get('/', userController.findAll)
+router.get('/:id', middleware.validId, middleware.validUser, userController.findById)
+router.patch('/:id', middleware.validId, middleware.validUser, userController.update)
 
 export default router
